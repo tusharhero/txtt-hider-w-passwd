4e53493b5844a531c15ddae188d2b26e78b364b1
tusharhero <tusharmaharana44676@gmail.com>
2022-04-14 14:05:00 +0530

    made basic ui

--- passwordhider/passwordhider/src/driver.py
+++ passwordhider/passwordhider/src/driver.py
@@ -2,7 +2,7 @@ from passwdhide import stringdecrypt
 from passwdhide import stringencrypt
 
 def encrypt(Message,password):
-    cid = open("unicode.txt",'rt')#always use the same cid for the the password i'e uncide.txt from this repo
+    cid = open("src/unicode.txt",'rt')#always use the same cid for the the password i'e uncide.txt from this repo
     cid = cid.read()
     dic = "abcdefghijklmnopqrstuvwxyz 1234567890.,"
     return stringencrypt(Message, password, dic, cid)
--- passwordhider/passwordhider/src/passwordhider/app.py
+++ passwordhider/passwordhider/src/passwordhider/app.py
@@ -5,9 +5,17 @@ import toga
 from toga.style import Pack
 from toga.style.pack import COLUMN, ROW
 from driver import encrypt
-
+#I know the code is trash but I am not going to explain it(for now)
 def win(start):
-    box = toga.box()
+    box = toga.Box()
+    tinput = toga.TextInput(placeholder="Enter the text.")
+    pinput = toga.TextInput(placeholder="Enter the Password.")
+    def output(dummy):
+        e = encrypt(tinput.value,pinput.value)
+        out = toga.Label(e)
+        box.add(out)
+    button = toga.Button("click me",on_press=output)
+    box.add(tinput,pinput,button)
     return box
 
 def main():
