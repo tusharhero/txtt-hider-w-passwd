c9a27406bd6acfefcc05bcaa8a556675a49bf3fc
tusharhero <tusharmaharana44676@gmail.com>
2022-04-15 23:16:41 +0530

    improved ui

--- app.py
+++ app.py
@@ -7,15 +7,15 @@ sg.theme('DarkBlue2')   # Add a touch of color
 
 # All the stuff inside your window.
 buttons = [sg.Button('Hide text'),sg.Button('Unhide text')]
+dialogs = [sg.Multiline('text',expand_x=True,expand_y=True,key='text'), sg.Multiline('Hidden text',expand_x=True,expand_y=True,key= 'htext')]
 layout = [
     [sg.Text('Only spaces, or uppercase or numbers and special characters other than ".,"')],
-    [sg.Text('text/hidden text:'), sg.InputText('text or hiddentext',expand_x=True)],
     [sg.Text('password:'), sg.InputText('password',expand_x=True)],
     buttons,
-    [[sg.Text('output:')],sg.Multiline('nothing here yet!',key='out',expand_x=True, expand_y=True)]
+    dialogs
     ]
 # Create the Window
-window = sg.Window('txtt-hider-w-passwd', layout,size=(750 ,300)) 
+window = sg.Window('txtt-hider-w-passwd', layout,size=(600 ,450)) 
 
 
 # Event Loop to process "events" and get the "values" of the inputs
@@ -23,8 +23,10 @@ while True:
     event, values = window.read()
     if event == sg.WIN_CLOSED: # if user closes window or clicks cancel
         break
+    #print(values)
+    #print(values[1])
     if event == 'Hide text':
-        window['out'].update(encrypt(values[0],values[1]))
+        window['htext'].update(encrypt(values['text'],values[0]))
     if event == 'Unhide text':
-        window['out'].update(decrypt(values[0],values[1]))
+        window['text'].update(decrypt(values['htext'],values[0]))
 window.close()
