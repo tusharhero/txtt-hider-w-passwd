59e1c6eb32468a6d9fc01b9a045b387d14df1433
tusharhero <tusharmaharana44676@gmail.com>
2022-08-30 23:48:26 +0530

    Reverat "refactoring"
    This reverts commit 6680b517bc924a4a6b1869b447852acd9399908e.

--- passwdhide.py
+++ passwdhide.py
@@ -1,10 +1,21 @@
 #/bin/python3
 #will take a  password and  a string and make the string only decodable by using the password
 def passwdnm(password,dic):#this function will take a passwd and dictionary and convert it into a number
-    nm = 1
-    for char in password:
-        nm += dic.index(char)+1 #the nuber of chracter + with the nm container + 1
-    return [nm,dic.index(password[-1])]
+    l = 0#counter
+    nm = 1#container
+    while l < len(password):#loops until the addition is complete
+        nm = nm + (c2l(password[l],dic)+1)#the nuber of chracter + with the nm container + 1
+        l = l + 1#counting 
+        #also outing the last character
+    return [nm,c2l(password[len(password)-1],dic)]
+
+def list2string(list):#converts list into string
+    strin = ''#string container
+    l = 0#counter
+    while l < len(list):#will loop until the word is completed
+        strin = strin + str(list[l])# add lth termt to the list
+        l = l + 1
+    return strin
 
 def stringencrypt(string,password,dic,cid):#this function will encrypt the text and return the encrypted text as a list.
     l = 0#good 'ol counter
@@ -12,10 +23,10 @@ def stringencrypt(string,password,dic,cid):#this function will encrypt the text 
     nml = passwdnm(password, dic)[len(passwdnm(password, dic))-1]#getting the last letter number in the password 
     cryptedstring = []#empty list as a container
     while l < len(string):#looping untill complete
-        mn = dic.string[l]*nm + nml #multiplying the character length with the passwordnumber and adding the last character number
+        mn = c2l(string[l],dic)*nm + nml #multiplying the character length with the passwordnumber and adding the last character number
         cryptedstring.append(cid[mn])#adding the new number's character in the spooking dic "cid" to the list
         l = l + 1#counting
-    return string(cryptedstring)# returns it as password list sa a stirng
+    return list2string(cryptedstring)# returns it as password list sa a stirng
 
 def stringdecrypt(cryptedstring,password,dic,cid):
     l = 0#good 'ol counter
@@ -27,4 +38,4 @@ def stringdecrypt(cryptedstring,password,dic,cid):
         mn = int(mn)#converting to int
         decryptedstring.append(dic[mn])#adding the mnth character from the list to the list
         l = l + 1#counting
-    return string(decryptedstring)
+    return list2string(decryptedstring)
